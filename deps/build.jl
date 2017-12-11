42f03f3bdc78be4300628dc1c9b71c20ef05b554
jingpengwu <jingpeng.wu@gmail.com>
2017-12-11 16:07:16 -0500

    add all compression backends; only support gcc in linux though

--- deps/build.jl
+++ deps/build.jl
@@ -2,28 +2,54 @@ using BinDeps
 
 vers = "1.12.1"
 
+# Set to true to build locally with internal compressors:
+build_locally_w_internal_compressors = true
+#build_locally_w_internal_compressors = false
+
 tagfile = "installed_vers"
 target = "libblosc.$(Libdl.dlext)"
-url = "https://bintray.com/artifact/download/julialang/generic/"
-
-if !isfile(target) || !isfile(tagfile) || readchomp(tagfile) != "$vers $(Sys.WORD_SIZE)"
-    if is_windows()
-        run(download_cmd(url*"libblosc$(Sys.WORD_SIZE)-$vers.dll", target))
-    elseif is_apple()
-        run(download_cmd(url*"libblosc$(Sys.WORD_SIZE)-$vers.dylib", target))
+if !isfile(tagfile) || readchomp(tagfile) != vers
+    if !build_locally_w_internal_compressors && is_windows()
+        run(download_cmd("http://ab-initio.mit.edu/blosc/libblosc$(Sys.WORD_SIZE)-$vers.dll", target))
+    elseif !build_locally_w_internal_compressors && is_apple()
+        run(download_cmd("http://ab-initio.mit.edu/blosc/libblosc$(Sys.WORD_SIZE)-$vers.dylib", target))
     else
         tarball = "c-blosc-$vers.tar.gz"
+        maindir = "c-blosc-$vers" 
         srcdir = "c-blosc-$vers/blosc"
         if !isfile(tarball)
             run(download_cmd("https://github.com/Blosc/c-blosc/archive/v$vers.tar.gz", tarball))
         end
         run(unpack_cmd(tarball, ".", ".gz", ".tar"))
-        cd(srcdir) do
-            println("Compiling libblosc...")
-            run(`$MAKE_CMD -f ../../make.blosc LIB=../../$target`)
+        if !build_locally_w_internal_compressors
+            cd(srcdir) do
+                println("Compiling libblosc...")
+                for f in ("blosc.c", "blosclz.c", "shuffle.c")
+                    println("   CC $f")
+                    run(`gcc -fPIC -O3 -msse2 -I. -c $f`)
+                end
+                println("   LINK libblosc")
+                run(`gcc -shared -o ../../$target blosc.o blosclz.o shuffle.o`)
+            end
+        else
+            cd(maindir) do
+                println("Compiling libblosc with internal compressor libs...")
+                run(`mkdir build`)
+                cd("build") do
+                    run(`cmake -DPREFER_EXTERNAL_COMPLIBS=OFF ..`)
+                    run(`make`)
+                    run(`cp blosc/$target ../..`)
+                    run(`make clean`)
+                end
+            end
+            run(`rm -rf $maindir $tarball`)
+            println("Compiled libblosc with internal compressor libs...")
         end
     end
     open(tagfile, "w") do f
         println(f, "$vers $(Sys.WORD_SIZE)")
-    end
+    end 
 end
+  
+            
+ 
